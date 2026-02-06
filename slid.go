8689324419e66158e7c6cbabb514bfbe003e8461
Oliver Kuckertz <oliver.kuckertz@authenticvision.com>
2026-02-06 14:20:32 +0100

    Add integer SLID type and conversion function
    This brings the SLID36 type closer to our internal schema/scan/slid.
    Useful in e.g. campaign-pages, where SLIDs are stored in-DB as ints.

--- slid.go
+++ slid.go
@@ -7,15 +7,33 @@ import (
 	"strings"
 )
 
+var ErrInvalidSLID = errors.New("not a positive 64-bit integer")
+
+type SLID int64
+
+func (slid SLID) String() string {
+	return slid.Base36()
+}
+
+func (slid SLID) Base10() string {
+	return strconv.FormatInt(int64(slid), 10)
+}
+
+func (slid SLID) Base36() string {
+	return strings.ToUpper(strconv.FormatInt(int64(slid), 36))
+}
+
+// SLID36 is a raw text SLID with base36-encoded contents
 type SLID36 string
 
-func NewCanonicalSLID36(slid36 string) (SLID36, error) {
-	slid, err := strconv.ParseInt(string(slid36), 36, 64)
+// SLID decodes the SLID for use as 64-bit integer
+func (slid SLID36) SLID() (SLID, error) {
+	i, err := strconv.ParseInt(string(slid), 36, 64)
 	if err != nil {
-		return "", fmt.Errorf("failed to parse SLID as base36: %w", err)
+		return 0, fmt.Errorf("failed to parse SLID as base36: %w", err)
 	}
-	if slid <= 0 {
-		return "", errors.New("not a positive 64-bit integer")
+	if i <= 0 {
+		return 0, ErrInvalidSLID
 	}
-	return SLID36(strings.ToUpper(strconv.FormatInt(slid, 36))), nil
+	return SLID(i), nil
 }
